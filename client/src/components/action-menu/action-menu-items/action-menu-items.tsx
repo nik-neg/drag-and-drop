dfbac65f9f35e73ac29ad5e31b2d24f6b0180d86
alex <alex.genkin.13@gmail.com>
2025-06-01 15:56:10 +0300

    action menu

---
+++ client/src/components/action-menu/action-menu-items/action-menu-items.tsx
@@ -0,0 +1,44 @@
+import { DropdownItem } from '@atlaskit/dropdown-menu';
+
+import { useCallback } from 'react';
+
+import { useBoardContext } from '@/pragmatic-drag-and-drop/documentation/examples/pieces/board/board-context';
+import { useColumnContext } from '@/pragmatic-drag-and-drop/documentation/examples/pieces/board/column-context';
+import { memo } from 'react';
+import { DropdownItemGroup } from '@atlaskit/dropdown-menu';
+
+export const ActionMenuItems = memo(() => {
+  const { columnId } = useColumnContext();
+  const { getColumns, reorderColumn } = useBoardContext();
+
+  const columns = getColumns();
+  const startIndex = columns.findIndex(column => column.columnId === columnId);
+
+  const moveLeft = useCallback(() => {
+    reorderColumn({
+      startIndex,
+      finishIndex: startIndex - 1,
+    });
+  }, [reorderColumn, startIndex]);
+
+  const moveRight = useCallback(() => {
+    reorderColumn({
+      startIndex,
+      finishIndex: startIndex + 1,
+    });
+  }, [reorderColumn, startIndex]);
+
+  const isMoveLeftDisabled = startIndex === 0;
+  const isMoveRightDisabled = startIndex === columns.length - 1;
+
+  return (
+    <DropdownItemGroup>
+      <DropdownItem onClick={moveLeft} isDisabled={isMoveLeftDisabled}>
+        Move left
+      </DropdownItem>
+      <DropdownItem onClick={moveRight} isDisabled={isMoveRightDisabled}>
+        Move right
+      </DropdownItem>
+    </DropdownItemGroup>
+  );
+});
