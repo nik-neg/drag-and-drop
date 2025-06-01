4f8e3884670e081b239b80aed49286d120f32fbb
alex <alex.genkin.13@gmail.com>
2025-06-01 17:51:42 +0300

    add avatars and role enum

---
+++ client/src/enums/role-enum.ts
@@ -0,0 +1,14 @@
+export enum RoleEnum {
+  DEVELOPER = 'Developer',
+  DESIGNER = 'Designer',
+  PRODUCT_MANAGER = 'Product Manager',
+  QA = 'QA',
+  HEAD_OF_PRODUCT = 'Head of Product',
+  HEAD_OF_DESIGN = 'Head of Design',
+  HEAD_OF_ENGINEERING = 'Head of Engineering',
+  HEAD_OF_QA = 'Head of QA',
+  HEAD_OF_DEVOPS = 'Head of DevOps',
+  HEAD_OF_SECURITY = 'Head of Security',
+  HEAD_OF_DATA = 'Head of Data',
+  HEAD_OF_BUSINESS = 'Head of Business',
+}
--- client/src/pragmatic-drag-and-drop/documentation/examples/data/people.ts
+++ client/src/pragmatic-drag-and-drop/documentation/examples/data/people.ts
@@ -1,3 +1,10 @@
+import { RoleEnum } from '@/enums/role-enum';
+import Alexander from './people/images/processed/Alexander';
+import Aliza from './people/images/processed/Aliza';
+import Alvin from './people/images/processed/Alvin';
+import Angie from './people/images/processed/Angie';
+import Arjun from './people/images/processed/Arjun';
+
 export type Person = {
   userId: string;
   name: string;
@@ -24,21 +31,21 @@ export const getBasicData = (): {
       columnId: 'column-1',
       title: 'To Do',
       items: [
-        { userId: '1', name: 'Task 1' },
-        { userId: '2', name: 'Task 2' },
+        { userId: '1', name: 'Task 1', role: RoleEnum.DEVELOPER, avatarUrl: Alexander },
+        { userId: '2', name: 'Task 2', role: RoleEnum.DEVELOPER, avatarUrl: Aliza },
       ],
     },
     'column-2': {
       columnId: 'column-2',
       title: 'In Progress',
-      items: [{ userId: '3', name: 'Task 3' }],
+      items: [{ userId: '3', name: 'Task 3', role: RoleEnum.DEVELOPER, avatarUrl: Alvin }],
     },
     'column-3': {
       columnId: 'column-3',
       title: 'Done',
       items: [
-        { userId: '4', name: 'Task 4' },
-        { userId: '5', name: 'Task 5' },
+        { userId: '4', name: 'Task 4', role: RoleEnum.PRODUCT_MANAGER, avatarUrl: Angie },
+        { userId: '5', name: 'Task 5', role: RoleEnum.HEAD_OF_DESIGN, avatarUrl: Arjun },
       ],
     },
   };
