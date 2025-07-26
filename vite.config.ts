487ed13379188bff024a1e7c80c0a864bece691f
alex <alex.genkin.13@gmail.com>
2025-07-26 22:21:44 +0300

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/drag-and-drop/',
   server: {
     port: 3000,
   },
