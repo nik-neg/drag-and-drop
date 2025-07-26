87e7b1717fa1997725b0070a7b74babfbbc7c187
alex <alex.genkin.13@gmail.com>
2025-07-26 21:28:21 +0300

    adjust board to center

--- src/components/board/board.styles.ts
+++ src/components/board/board.styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const SBoardContainer = styled.div`
+  min-height: 100vh;
+  width: 100%;
   display: flex;
+  align-items: center;
+  justify-content: center;
   gap: 16px;
   padding: 16px;
-  min-height: 100vh;
   background: #f4f5f7;
 `;
